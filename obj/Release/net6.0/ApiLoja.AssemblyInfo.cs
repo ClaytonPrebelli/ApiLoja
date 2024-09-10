9c72304ccf1d06ddb2f638eca8ea2655e2dcd1b3
Clayton Prebelli Pires <clayton.pires@actdigital.com>
2024-09-10 14:43:24 -0300

    update

--- obj/Release/net6.0/ApiLoja.AssemblyInfo.cs
+++ obj/Release/net6.0/ApiLoja.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLoja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ae25d58dbd9d5d5abb3ecdd2c92a21aec59d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb12febcdf48ee90b6dac6070d46871143a82cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLoja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiLoja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
